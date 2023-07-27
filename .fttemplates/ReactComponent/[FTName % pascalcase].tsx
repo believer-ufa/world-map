258d042eb47c3d6fc103f2f890687ab5fd834d38
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2023-07-28 05:57:28 +0600

    #upd react components templates

--- .fttemplates/ReactComponent/[FTName % pascalcase].tsx
+++ .fttemplates/ReactComponent/[FTName % pascalcase].tsx
@@ -1,19 +1,17 @@
-import { memo } from 'react';
+import { ReactNode, memo } from 'react';
 import cn from 'classnames';
-
-import useFm from '@/hooks/useFm';
 
 import classes from './[FTName | pascalcase].module.scss';
 
-import messages from './messages';
-
-import { <FTName | pascalcase>Props } from './types';
+export interface <FTName | pascalcase>Props {
+  className?: string;
+  children?: ReactNode;
+}
 
 export const <FTName | pascalcase> = memo<<FTName | pascalcase>Props>(({ className }) => {
-  const fm = useFm();
   return (
     <div className={cn(classes.container, className)}>
-      {fm(messages.title)}
+      <FTName | pascalcase> component
     </div>
   );
 });
--- .fttemplates/ReactComponentWithForwardRef/types.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface <FTName | pascalcase>Props {
-  className?: string;
-  children?: ReactNode;
-}
