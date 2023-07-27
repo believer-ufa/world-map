e1df0d7f84f5b301e94983af5bd309d476280f08
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2023-07-28 05:09:21 +0600

    #upd additional eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // 'plugin:@typescript-eslint/strict-type-checked' - could be useful
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'airbnb',
   ],
@@ -20,6 +23,12 @@ module.exports = {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
   rules: {
     '@typescript-eslint/no-unused-vars': 0,
